40cd1cb1942395bbcae869565fb72987cc9bbb3a
ghetolay <ghetolay@gmail.com>
2016-08-30 12:23:49 +0200

    static animation

--- src/AccordionGroup.ts
+++ src/AccordionGroup.ts
@@ -1,29 +1,43 @@
-import {Component, Input, Host, forwardRef, Inject, ContentChild, ElementRef, ViewChild} from "@angular/core";
+import {Component, Input, Host, forwardRef, Inject, ContentChild, ElementRef, ViewChild,
+    trigger, state, transition, style, animate } from "@angular/core";
 import {Accordion} from "./Accordion";
 import {AccordionToggle} from "./AccordionToggle";
 
 @Component({
     selector: "accordion-group",
+    styles: [`
+        .panel-collapse{
+            overflow: hidden;
+        }
+    `],
     template: `
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" (click)="checkAndToggle()">
-      <h4 class="panel-title">
-        <a *ngIf="heading" role="button" data-toggle="collapse" [attr.aria-expanded]="isOpened">
-            {{ heading }}
-        </a>
-        <ng-content select="accordion-heading"></ng-content>
-        <div class="caret" [ngStyle]="{ display: accordion.showArrows ? '' : 'none', borderTop: isOpened ? '0' : '4px dashed', borderBottom: isOpened ? '4px dashed' : '0' }">
+      <div class="panel panel-default">
+        <div class="panel-heading" role="tab" (click)="checkAndToggle()">
+          <h4 class="panel-title">
+            <a *ngIf="heading" role="button" data-toggle="collapse" [attr.aria-expanded]="isOpened">
+                {{ heading }}
+            </a>
+            <ng-content select="accordion-heading"></ng-content>
+            <div class="caret" [ngStyle]="{ display: accordion.showArrows ? '' : 'none', borderTop: isOpened ? '0' : '4px dashed', borderBottom: isOpened ? '4px dashed' : '0' }">
+            </div>
+          </h4>
         </div>
-      </h4>
-    </div>
-    <div  class="panel-collapse collapse" [class.in]="isOpened" role="tabpanel" [attr.aria-labelledby]="heading">
-      <div class="panel-body">
-        <ng-content></ng-content>
+        <div *ngIf="isOpened" class="panel-collapse" role="tabpanel" [attr.aria-labelledby]="heading" [@tabState]="in">
+          <div class="panel-body">
+            <ng-content></ng-content>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-`
-})
+    `,
+    animations: [
+        trigger("tabState", [
+          state("void",   style({
+              height: "0"
+          })),
+          transition("void <=> *", animate("350ms ease"))
+        ])
+      ]
+    })
 export class AccordionGroup {
 
     @Input()
